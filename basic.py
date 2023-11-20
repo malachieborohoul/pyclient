d969e9d7314699e57f5b334cef174de7efddaa73
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-20 22:00:14 +0100

    Endpoint

--- basic.py
+++ basic.py
@@ -1,7 +1,7 @@
 import requests
 # endpoint="https://httpbin.org/status/200"
 endpoint="https://httpbin.org/anything"
-endpoint="http://127.0.0.1:8000/"
+endpoint="http://127.0.0.1:8000/api"
 
 get_response=requests.get(endpoint, json={'query':'Hello world'})
 
