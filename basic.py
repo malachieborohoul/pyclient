b4c256fd475f6cc6411a330cb331315386abf2a5
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-20 21:26:25 +0100

    Add endpoints getresponse

--- basic.py
+++ basic.py
@@ -2,6 +2,6 @@ import requests
 # endpoint="https://httpbin.org/status/200"
 endpoint="https://httpbin.org/anything"
 
-get_response=requests.get(endpoint, json={'query':'Hello world'})
+get_response=requests.get(endpoint, data={'query':'Hello world'})
 
 print(get_response.text)
